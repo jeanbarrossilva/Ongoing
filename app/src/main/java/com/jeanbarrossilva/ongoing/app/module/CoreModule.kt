4e1a7087b097292329b3e088c2b32183f8d172ce
Jean Silva <jeanbarrossilva@outlook.com>
2022-11-06 11:12:34 -0300

    refact: remove :platform-session

--- app/src/main/java/com/jeanbarrossilva/ongoing/app/module/CoreModule.kt
+++ app/src/main/java/com/jeanbarrossilva/ongoing/app/module/CoreModule.kt
@@ -2,18 +2,17 @@ package com.jeanbarrossilva.ongoing.app.module
 
 import com.jeanbarrossilva.ongoing.core.registry.ActivityRegistry
 import com.jeanbarrossilva.ongoing.core.session.SessionManager
+import com.jeanbarrossilva.ongoing.core.session.inmemory.InMemorySessionManager
 import com.jeanbarrossilva.ongoing.core.session.inmemory.InMemoryUserRepository
 import com.jeanbarrossilva.ongoing.core.session.user.UserRepository
 import com.jeanbarrossilva.ongoing.platform.registry.authorization.CurrentUserIdProvider
 import com.jeanbarrossilva.ongoing.platform.registry.extensions.database
 import com.jeanbarrossilva.ongoing.platform.registry.extensions.getActivityRegistry
 import com.jeanbarrossilva.ongoing.platform.registry.extensions.invoke
-import com.jeanbarrossilva.ongoing.platform.session.GoogleSignInSessionManager
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 internal val coreModule = module {
-    single<SessionManager> { GoogleSignInSessionManager(androidContext()) }
+    single<SessionManager> { InMemorySessionManager() }
     single<UserRepository> { InMemoryUserRepository(sessionManager = get()) }
     single<ActivityRegistry> {
         database.getActivityRegistry(CurrentUserIdProvider(sessionManager = get()))

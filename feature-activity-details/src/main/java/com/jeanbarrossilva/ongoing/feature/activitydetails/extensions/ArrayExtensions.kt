bcdb19def8d6fe352e4010f2acaa3d47988b2b33
Jean Silva <jeanbarrossilva@outlook.com>
2022-11-24 20:53:17 -0300

    refact: remove unused arrayOfNotNull

--- feature-activity-details/src/main/java/com/jeanbarrossilva/ongoing/feature/activitydetails/extensions/ArrayExtensions.kt
+++
@@ -1,5 +0,0 @@
-package com.jeanbarrossilva.ongoing.feature.activitydetails.extensions
-
-internal inline fun <reified T> arrayOfNotNull(vararg elements: T): Array<T> {
-    return arrayOf(*elements).filter { it == null }.toTypedArray()
-}

30a1fd65e98e20c2c9cea5717a28ce5da691b565
Jean Silva <jeanbarrossilva@outlook.com>
2022-12-19 04:52:39 -0300

    refact: incorrect ActivitiesSelection.Off test name

--- feature-activities/src/test/java/com/jeanbarrossilva/ongoing/feature/activities/selection/OnActivitiesSelectionTests.kt
+++ feature-activities/src/test/java/com/jeanbarrossilva/ongoing/feature/activities/selection/OnActivitiesSelectionTests.kt
@@ -12,7 +12,7 @@ import org.junit.Test
 
 internal class OnActivitiesSelectionTests {
     @Test
-    fun `GIVEN an unselected activity WHEN checking if it is selected THEN it is`() {
+    fun `GIVEN an unselected activity WHEN checking if it is selected THEN it isn't`() {
         assertFalse(ActivitiesSelection.On().isSelected(ContextualActivity.sample))
     }
 
